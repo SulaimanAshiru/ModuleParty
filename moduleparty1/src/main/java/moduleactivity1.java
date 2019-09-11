ce7f059ec1fe0e6d5339a1e1716fb1e10baca8ac
sulaimanashiru <dash.ashiru@gmail.com>
2019-09-11 10:27:37 -0400

    Initial commit

---
+++ moduleparty1/src/main/java/moduleactivity1.java
@@ -0,0 +1,15 @@
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.moduleparty1.R;
+
+public class moduleactivity1 extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moduleparty1);
+    }
+}
---
+++ moduleparty2/src/main/java/com/example/partymodule2/ModuleActivity2.java
@@ -0,0 +1,15 @@
+package com.example.partymodule2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModuleActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partymodules2);
+
+    }
+}
